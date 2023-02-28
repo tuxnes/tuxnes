tuxnes/tuxnes

commit 2da25733c46d92a7d89b6ad74de65c9445e72187

Use a safer method to parse the basename
The old code assumes the filename has a dot somewhere after the last
slash.  If not, and there is a dot somewhere before the last slash, then
the length underflows.

src/emu.c
@@ -1066,7 +1066,6 @@ main (int argc, char **argv)
 {
   int x;
   int r;
-  int basestart, baseend; /* these are for working out the base filename */
   int audiofd;
   int size;
   int cmirror;
@@ -1456,30 +1455,33 @@ main (int argc, char **argv)
     parsing of leading directory information as well as the trailing extension
     (.nes, .gz, .Z).
   */
-  basestart = baseend = 0;
-  for (size_t i = 0; i < strlen(filename); i++)
-    {
-      if (filename[i] == '/')
-        {
-          basestart = i + 1;
-        }
-      else if (filename[i] == '.')
-        {
-          baseend = i - 1;
-        }
-    }
+  {
+    char *basename;
+    size_t baselen, namelen;
 
-    if (! (basefilename = malloc(baseend - basestart + 1)))
+    basename = filename;
+    baselen = namelen = strlen(filename);
+    for (size_t i = 0; i < namelen; i++)
+      {
+        if (filename[i] == '/')
+          {
+            basename = &filename[i + 1];
+            baselen = namelen - i - 1;
+          }
+        else if (filename[i] == '.')
+          {
+            baselen -= namelen - i;
+          }
+      }
+
+    if (! (basefilename = malloc(baselen + 1)))
       {
         perror ("main: malloc");
         exit (1);
       }
-
-    for (size_t i = 0; i <= (baseend - basestart); i++)
-      {
-        basefilename[i] = filename[basestart + i];
-      }
-    basefilename[baseend - basestart + 1] = '\0';
+    strncpy(basefilename, basename, baselen);
+    basefilename[baselen] = '\0';
+  }
 
   /* initialize joysticks */
   js_init();

commit 53cfdd22dcf05ff346fa5e3f39f3bf399ca8dfcc

Fix for malloc failure caused by loading ROM filename with extra dot.
Appears to have been regressed by 2da25733c46d92a7d89b6ad74de65c9445e72187.

Reading that change I think they could have done a better job using strrchr.

src/emu.c
@@ -1329,7 +1329,7 @@ main(int argc, char **argv)
 				basename = &filename[i + 1];
 				baselen = namelen - i - 1;
 			} else if (filename[i] == '.') {
-				baselen -= namelen - i;
+				baselen = namelen - i;
 			}
 		}
 